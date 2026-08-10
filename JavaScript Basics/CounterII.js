f4c7db8c409dd5264db305750905f05ddc2a9883
Maheen Raza <124949405+maheenraza03@users.noreply.github.com>
2026-08-10 21:22:06 +0000

    another basic one

---
+++ JavaScript Basics/CounterII.js
@@ -0,0 +1,35 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    original = init;
+
+    function increment() {
+        init = init + 1;
+        return init;
+    }
+
+    function decrement() {
+        init = init - 1;
+        return init;
+    }
+
+    function reset() {
+        init = original
+        return init;
+    }
+
+    return {
+        increment: increment,
+        decrement: decrement,
+        reset: reset
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
